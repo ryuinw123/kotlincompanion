f8a9184fd4a92f4191cac05aca2a9543eaad8317
Film suphanut <nongfilm2987@gmail.com>
2022-09-27 03:12:32 +0700

    update fix branch

--- app/src/main/java/com/example/kmitlcompanion/MainActivity.kt
+++ app/src/main/java/com/example/kmitlcompanion/MainActivity.kt
@@ -10,6 +10,7 @@ import androidx.navigation.NavController
 import androidx.navigation.findNavController
 import androidx.navigation.fragment.NavHostFragment
 import androidx.navigation.fragment.findNavController
+import com.example.kmitlcompanion.fragments.MainFragment
 
 
 class MainActivity : AppCompatActivity() {
@@ -19,15 +20,11 @@ class MainActivity : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
-        val navHostFragment = supportFragmentManager.findFragmentById(R.id.fragment) as NavHostFragment
-        navController = navHostFragment.navController
-
-//        if (savedInstanceState == null) {
-//            supportFragmentManager.beginTransaction()
-//                .replace(R.id.container, MainFragment.newInstance())
-//                //.replace(R.id.container, SignIn.newInstance())
-//                .commitNow()
-//        }
+        if (savedInstanceState == null) {
+            supportFragmentManager.beginTransaction()
+                .replace(R.id.container, MainFragment.newInstance())
+                .commitNow()
+        }
     }
 
 
--- app/src/main/java/com/example/kmitlcompanion/viewmodels/SignInViewModel.kt
+++
@@ -1,9 +0,0 @@
-package com.example.kmitlcompanion.viewmodels
-
-import androidx.lifecycle.ViewModel
-
-class SignInViewModel : ViewModel() {
-    //ใช้เก็บข้อมูล ตัวแปร บลาๆ
-
-    // TODO: Implement the ViewModel
-}
