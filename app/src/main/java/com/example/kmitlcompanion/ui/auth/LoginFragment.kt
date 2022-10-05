96e59691e6bfacc4072d0179e07d66bfa1529c23
Film suphanut <nongfilm2987@gmail.com>
2022-10-05 13:51:09 +0700

    update login page

--- app/src/main/java/com/example/kmitlcompanion/ui/auth/LoginFragment.kt
+++ app/src/main/java/com/example/kmitlcompanion/ui/auth/LoginFragment.kt
@@ -1,34 +1,40 @@
 package com.example.kmitlcompanion.ui.auth
 
-import androidx.lifecycle.ViewModelProvider
 import android.os.Bundle
-import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
-import com.example.kmitlcompanion.R
-import com.example.kmitlcompanion.presentation.LoginViewModel
+import androidx.appcompat.app.AppCompatActivity
+import androidx.fragment.app.Fragment
+import androidx.navigation.findNavController
+import androidx.navigation.fragment.findNavController
 import com.example.kmitlcompanion.databinding.FragmentLoginBinding
+import com.example.kmitlcompanion.presentation.LoginViewModel
+import com.example.kmitlcompanion.ui.mainfragment.MainFragmentDirections
 
 class LoginFragment : Fragment() {
 
     private lateinit var viewModel: LoginViewModel
     private lateinit var binding: FragmentLoginBinding
 
-    companion object {
-        fun newInstance() = LoginFragment()
-    }
-
-
-
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View? {
+        (activity as AppCompatActivity?)?.getSupportActionBar()?.hide()
+        binding = FragmentLoginBinding.inflate(inflater,container, false)
 
+        binding.signinButton.setOnClickListener{
+            view?.findNavController()?.navigate(LoginFragmentDirections.actionLoginFragmentToMapboxFragment2())
+        }
 
-        return inflater.inflate(R.layout.fragment_login, container, false)
+        return binding.root
     }
+
+//    companion object {
+//        fun newInstance() = LoginFragment()
+//    }
+//
 
 //    override fun onActivityCreated(savedInstanceState: Bundle?) {
 //        super.onActivityCreated(savedInstanceState)
