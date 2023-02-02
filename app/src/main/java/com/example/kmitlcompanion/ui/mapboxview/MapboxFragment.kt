2e46c5084478280cb6a42e7328c9e4fbe2e0aea6
Film suphanut <nongfilm2987@gmail.com>
2023-02-02 16:16:58 +0700

    Update MapboxFragment.kt

--- app/src/main/java/com/example/kmitlcompanion/ui/mapboxview/MapboxFragment.kt
+++ app/src/main/java/com/example/kmitlcompanion/ui/mapboxview/MapboxFragment.kt
@@ -81,7 +81,7 @@ class MapboxFragment : BaseFragment<FragmentMapboxBinding, MapboxViewModel>() {
     private fun FragmentMapboxBinding.setupViewObservers(){
         lifecycle.addObserver(helper.map)
         lifecycle.addObserver(helper.location)
-        //lifecycle.addObserver(helper.navigation)
+        lifecycle.addObserver(helper.navigation)
         this@MapboxFragment.viewModel.run {
             mapInformationResponse.observe(viewLifecycleOwner, Observer { information ->
                 this@MapboxFragment.context?.let {
@@ -186,30 +186,30 @@ class MapboxFragment : BaseFragment<FragmentMapboxBinding, MapboxViewModel>() {
 
 
             //For Nataviation
-//            applicationMode.observe(viewLifecycleOwner , Observer {
-//                bottomBarUtils.applicationMode = it
-//                if (it == 0) {
-//                    Log.d("Navigation" ,"End Navigation")
-//                    helper.navigation.stopNavigationEvent()
-//                }
-//                else if (it == 1) {
-//                    Log.d("Navigation","Enter ApplicationMode 1")
-//                    helper.navigation.startNavigation(requireContext())
-//                }
-//            })
-//
-//
-//            recenterEvent.observe(viewLifecycleOwner, Observer {
-//                helper.navigation.recenterEvent()
-//            })
-//
-//            routeOverViewEvent.observe(viewLifecycleOwner , Observer {
-//                helper.navigation.routeOverviewEvent()
-//            })
-//
-//            soundEvent.observe(viewLifecycleOwner, Observer {
-//                helper.navigation.soundEvent()
-//            })
+            applicationMode.observe(viewLifecycleOwner , Observer {
+                bottomBarUtils.applicationMode = it
+                if (it == 0) {
+                    Log.d("Navigation" ,"End Navigation")
+                    helper.navigation.stopNavigationEvent()
+                }
+                else if (it == 1) {
+                    Log.d("Navigation","Enter ApplicationMode 1")
+                    helper.navigation.startNavigation(requireContext())
+                }
+            })
+
+
+            recenterEvent.observe(viewLifecycleOwner, Observer {
+                helper.navigation.recenterEvent()
+            })
+
+            routeOverViewEvent.observe(viewLifecycleOwner , Observer {
+                helper.navigation.routeOverviewEvent()
+            })
+
+            soundEvent.observe(viewLifecycleOwner, Observer {
+                helper.navigation.soundEvent()
+            })
 
             locationIcon.observe(viewLifecycleOwner, Observer {
                 helper.location.updatePuckIcon(requireContext(),it)
